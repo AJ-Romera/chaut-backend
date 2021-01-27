9ca690b21fa1f2e0b3f21c592074a7e39424bbdf
AJ-Romera <ajromeraalfonso@gmail.com>
2021-01-27 19:06:01 +0100

    CHNG: Create dbMessages.js. DB config Connection & New Post Route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // importing
 import express from 'express';
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
 
 // app config
 const app = express();
@@ -8,11 +10,31 @@ const port = process.env.PORT || 9000;
 // middleware
 
 // DB config
+const connection_url =
+    'mongodb+srv://admin:nmL9IrOoUeWBpgMd@cluster0.5cuiv.mongodb.net/chautdb?retryWrites=true&w=majority';
+
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 // ????
 
 // api routes
 app.get('/', (req, res) => res.status(200).send('hello world'));
 
-// listener
+app.post('/api/v1/messages/new', (req, res) => {
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+// listen
 app.listen(port, () => console.log(`Listening on localhost: ${port}`));
